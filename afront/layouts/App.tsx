fa2877a07714a83ec51536f20463547f37b54f5e
naerim <naerim1119@gmail.com>
2021-03-23 13:25:07 +0900

    코드 스플리팅, 회원가입 페이지 UI

--- afront/layouts/App.tsx
+++ afront/layouts/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Switch, Route, Redirect, Router } from 'react-router-dom';
-import LogIn from '@pages/Login';
-import SignUp from '@pages/SignUp';
+import loadable from '@loadable/component';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+const LogIn = loadable(() => import('@pages/Login'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
   return (
